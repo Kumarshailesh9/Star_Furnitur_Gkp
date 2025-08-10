07cef9c76c686992e4f64607e74e6663e9e3be55
kumarshailesh9 <theshaileshkuma9@gmail.com>
2025-08-11 00:01:22 +0530

    update product 2

--- app/all-product/sofa/page.tsx
+++ app/all-product/sofa/page.tsx
@@ -3,7 +3,7 @@ const DoubleBeds = () => {
    
 
     return (
-        <div className='h-full flex justify-center items-center'>
+        <div className='h-[400px] flex justify-center items-center'>
             <h1>No product found</h1>
         </div>
     );
